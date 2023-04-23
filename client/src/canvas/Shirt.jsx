a0915a512286bd5e339addeebd9b7a0beae3fdd1
Hoàng Tấn <tanht2@vng.com.vn>
2023-04-23 23:35:25 +0700

    chore: update Hoodie file

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { easing } from 'maath';
-import { useSnapshot } from 'valtio';
-import { useFrame } from '@react-three/fiber';
-import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+import { useFrame } from '@react-three/fiber'
+import { Decal, useGLTF, useTexture } from '@react-three/drei'
 
-import state from '../store';
+import state from '../store'
 
 const Shirt = () => {
-  const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF('/shirt_baked.glb')
 
-  const logoTexture = useTexture(snap.logoDecal);
-  const fullTexture = useTexture(snap.fullDecal);
+  const logoTexture = useTexture(snap.logoDecal)
+  const fullTexture = useTexture(snap.fullDecal)
 
-  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta))
 
-  const stateString = JSON.stringify(snap);
+  const stateString = JSON.stringify(snap)
 
   return (
     <group key={stateString}>
@@ -26,17 +26,10 @@ const Shirt = () => {
         material-roughness={1}
         dispose={null}
       >
-        {snap.isFullTexture && (
-          <Decal 
-            position={[0, 0, 0]}
-            rotation={[0, 0, 0]}
-            scale={1}
-            map={fullTexture}
-          />
-        )}
+        {snap.isFullTexture && <Decal position={[0, 0, 0]} rotation={[0, 0, 0]} scale={1} map={fullTexture} />}
 
         {snap.isLogoTexture && (
-          <Decal 
+          <Decal
             position={[0, 0.04, 0.15]}
             rotation={[0, 0, 0]}
             scale={0.15}
